9a9618fb5ee5d2fd458659cf2cf2e185733f5f2f
YOUR NAME <YOUR@EMAIL.com>
2017-10-18 22:15:28 +0800

    added shorten method and validations in model

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,3 +1,12 @@
 class Url < ApplicationRecord
-	
+	def shorten
+		short=(('a'..'z').to_a+ ('A'..'Z').to_a+ (0..9).to_a).sample(7).join
+		@url= Url.new(long_url: params["long_url"], short_url: short)
+		@url.save	
+	end
+
+	validates :long_url, presence: true
+	validates :long_url, uniqueness: {case_sensitive: false, message: "Was shortened before"}
+
+	validates :long_url, format: {with: (URI::regexp(['http', 'https'])), message: "Format is not good"}
 end
